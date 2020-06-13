64e98372419cbad1b60286311cd22ac7fa5cd71d
Roman Isaikin <romix.lab@gmail.com>
2020-06-13 11:17:52 +0200

    Fix rprint macro (terminal selection).

--- rtt-target/src/print.rs
+++ rtt-target/src/print.rs
@@ -124,17 +124,17 @@ pub mod print_impl {
 /// range from 0 to 15.
 #[macro_export]
 macro_rules! rprint {
-    ($s:expr) => {
-        $crate::print_impl::write_str(0, $s);
-    };
-    ($($arg:tt)*) => {
-        $crate::print_impl::write_fmt(0, format_args!($($arg)*));
-    };
     (=> $terminal:expr, $s:expr) => {
         $crate::print_impl::write_str($terminal, $s);
     };
     (=> $terminal:expr, $($arg:tt)*) => {
         $crate::print_impl::write_fmt($terminal, format_args!($($arg)*));
+    };
+    ($s:expr) => {
+        $crate::print_impl::write_str(0, $s);
+    };
+    ($($arg:tt)*) => {
+        $crate::print_impl::write_fmt(0, format_args!($($arg)*));
     };
 }
 
